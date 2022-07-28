c29c457b236db6204afcee425396b13085ea23e0
Rodrigo Viturino <viturino_souza@outlook.com>
2022-07-28 14:48:15 -0300

    Estruturando o site html e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import React from 'react';
+
+import styles from './App.module.css';
+import './global.css';
+
 export function App(){
   return(
-    <h1>Hello World!</h1>
+    <div>
+
+      <header className={styles.header}>
+        <img src="./assets/rocket.svg" alt="Logo" />
+        <h1>todo</h1>
+      </header>
+
+      
+
+    </div>
   );
 }
