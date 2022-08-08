4eff372c490ffb5f688b0971c7eb5720ba2b3ef8
Rodrigo Viturino <viturino_souza@outlook.com>
2022-08-08 19:51:21 -0300

    ajuste no form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, InputEvent } from 'react';
 import { PlusCircle } from 'phosphor-react';
 
 import styles from './Form.module.css';
 
 export function Form(){
+
+  // Lista de Tarefas
+  const [ tasks, setTasks ] = React.useState([
+    'Estudar JS',
+    'Comprar Ingresso São Paulo FC',
+    'Fazer Caminhada'
+  ]);
+
+  // Valor do input de tarefa
+  const [newTask, setNewTask] = React.useState('');
+
+  function handleCreateTask(event: FormEvent){
+    event.preventDefault();
+
+    setTasks([...tasks, newTask]);
+
+  }
+
+  function handleNewTaskChange(event: InputEvent<HTMLInputElement>){
+    // event.target.setCustomValidity('');
+    setNewTask(event.target.value);
+  }
+
+
   return (
-    <form action="#" method="post" className={styles.form}>
-      <input type="text" id="tarefa" name="tarefa" placeholder='Adicione uma nova tarefa'/>
+    <form onSubmit={handleCreateTask} className={styles.form}>
+
+      <input
+        name="tarefa"
+        placeholder="Adicione uma nova tarefa"
+        value={newTask}
+        onChange={handleNewTaskChange}
+      />
+
       <button type="submit">Criar <PlusCircle size={20} /></button>
+
     </form>
   )
 }
